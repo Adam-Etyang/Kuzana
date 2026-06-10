e8f6c4d63efe3b0e81623bc1bd9fd0728a43cbfc
Adam-Etyang <adametyang69@icloud.com>
2026-06-10 09:47:25 +0300

    Profile service scaffold

--- server/src/profile/profile.module.ts
+++ server/src/profile/profile.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProfileController } from './profile.controller.js';
 import { ProfileService } from './profile.service.js';
+import {prisma} from '../../lib/prisma.js';
 
 @Module({
   controllers: [ProfileController],
-  providers: [ProfileService]
+  providers: [ProfileService,
+    {
+      provide: 'PRISMA',
+      useValue: prisma,
+    }
+  ]
 })
 export class ProfileModule {}
