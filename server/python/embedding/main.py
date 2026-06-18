07ae968f2cec1ee1a8e952dc8b5283972bcc8209
Adam-Etyang <adametyang69@icloud.com>
2026-06-19 00:39:41 +0300

    Bug fixes

--- server/python/embedding/main.py
+++ server/python/embedding/main.py
@@ -1,13 +1,16 @@
 import os
 from google import genai
+from dotenv import load_dotenv
+load_dotenv()
+
 
 client = genai.Client(api_key = os.getenv("Embedding_API_Key"))
-
-async def getEmbeddings(text:str):
+async def getEmbeddings(text: str):
     embedding = client.models.embed_content(
-            model='gemini-embedding-2',
-            contents=text
-            )
-    return embedding 
+        model='gemini-embedding-2',
+        contents=text
+    )
+    return embedding.embeddings[0].values
+
 
 
