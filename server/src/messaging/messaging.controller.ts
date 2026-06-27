d2ceac83c27a9b870523b6505303bcf0999a6b8b
Adam-Etyang <adametyang69@icloud.com>
2026-06-27 12:53:37 +0300

    commit

--- server/src/messaging/messaging.controller.ts
+++ server/src/messaging/messaging.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  UseGuards,
+  Controller,
+  Body,
+  Get,
+  Post,
+  Put,
+  Param,
+  Req,
+} from '@nestjs/common';
+import {AuthGuard} from '@thallesp/nestjs-better-auth';
+import { MessagingService } from './messaging.service';
+
 
 @Controller('messaging')
-export class MessagingController {}
+export class MessagingController {
+  constructor(private readonly messagingService: MessagingService) {}
+
+  //send message endpoint
+  @Post('send')
+  async sendMessage(@Req() req: any, @Body() body: any) {
+    return this.messagingService.sendMessage(req.user.id, body);
+  }
+
+  //get messages endpoint
+  @Get('get/:id')
+  async getMessages(@Param('id') id:string){
+    return await this.messagingService.getMessages(id);
+  }
+
+}
