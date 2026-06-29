cfec2961868a18852632cd00f5e1879e04d6f522
Adam-Etyang <adametyang69@icloud.com>
2026-06-29 11:00:07 +0300

    Commit

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
 import { AuthModule } from '@thallesp/nestjs-better-auth';
-import { auth } from '../lib/auth.js'; 
+import { auth } from '../lib/auth.js';
 import {prisma} from '../lib/prisma.js';
 import { UsersModule } from './users/users.module.js';
 import { ProfileModule } from './profile/profile.module.js';
 import { MatchingModule } from './matching/matching.module.js';
 import { MessagingModule } from './messaging/messaging.module.js';
+import { RequestsModule } from './requests/requests.module.js';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { MessagingModule } from './messaging/messaging.module.js';
     ProfileModule,
     MatchingModule,
     MessagingModule,
+    RequestsModule,
   ],
   controllers: [AppController],
   providers: [AppService,{provide: 'PRISMA', useValue: prisma,}],
